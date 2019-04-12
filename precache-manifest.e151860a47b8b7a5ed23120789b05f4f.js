e6836f04f20136c7539d3fac24caf15c4d9f899e
William Kwok <wkwok1998@gmail.com>
2019-04-11 18:14:18 -0700

    Updates

---
+++ precache-manifest.e151860a47b8b7a5ed23120789b05f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96dc543c4d32a89bb13c",
+    "url": "/pwatest-wk/static/css/main.ee04ca90.chunk.css"
+  },
+  {
+    "revision": "96dc543c4d32a89bb13c",
+    "url": "/pwatest-wk/static/js/main.7d559a9f.chunk.js"
+  },
+  {
+    "revision": "c9bd710cf574ba4c4cff",
+    "url": "/pwatest-wk/static/js/runtime~main.1baf21ba.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/pwatest-wk/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwatest-wk/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "28fa10ef6c9e09fd3f0cfb0315883bf7",
+    "url": "/pwatest-wk/index.html"
+  }
+];
